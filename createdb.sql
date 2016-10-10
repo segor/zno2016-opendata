bad0f75261b7962bca244affadea6a6bf61d75cf
segor <serghei.gorodetki@gmail.com>
2016-10-10 17:12:42 +0300

    remove new line symbols from data

--- createdb.sql
+++ createdb.sql
@@ -97,8 +97,7 @@ CREATE FUNCTION [dbo].[clean_str]
 RETURNS NVARCHAR(500)
 AS
 BEGIN
-	RETURN CASE WHEN @str <> 'null' THEN REPLACE(@str, '"', '') END
-
+	RETURN CASE WHEN @str <> 'null' THEN LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(@str, '"', ''), CHAR(13), N''), CHAR(10), N' '))) END
 END
 
 GO
